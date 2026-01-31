491a4eff7b0ab15aee97586946fe12a651a2ab07
Jacob Scherrebeck <35434108+jascdk@users.noreply.github.com>
2026-02-01 00:57:17 +0100

    Remove generation feature

--- esphome/components/wavinahc9000v3/binary_sensor.py
+++ esphome/components/wavinahc9000v3/binary_sensor.py
@@ -10,12 +10,9 @@ CONF_TYPE = "type"
 CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend(
     {
         cv.GenerateID("wavinahc9000v3_id"): cv.use_id(WavinAHC9000),
-        cv.Required(CONF_TYPE): cv.one_of("yaml_ready", lower=True),
+        # No types left
     }
 )
 
 async def to_code(config):
-    hub = await cg.get_variable(config[CONF_PARENT_ID])
-    bs = await binary_sensor.new_binary_sensor(config)
-    if config[CONF_TYPE] == "yaml_ready":
-        cg.add(hub.set_yaml_ready_binary_sensor(bs))
+    pass
--- esphome/components/wavinahc9000v3/text_sensor.py
+++ esphome/components/wavinahc9000v3/text_sensor.py
@@ -7,7 +7,6 @@ from . import WavinAHC9000
 CONF_PARENT_ID = "wavinahc9000v3_id"
 CONF_TYPE = "type"
 
-TYPE_YAML = "yaml_suggestion"
 TYPE_SOFTWARE_VERSION = "software_version"
 TYPE_HARDWARE_VERSION = "hardware_version"
 TYPE_DEVICE_NAME = "device_name"
@@ -15,7 +14,7 @@ TYPE_DEVICE_NAME = "device_name"
 CONFIG_SCHEMA = text_sensor.text_sensor_schema().extend(
     {
         cv.GenerateID("wavinahc9000v3_id"): cv.use_id(WavinAHC9000),
-        cv.Optional(CONF_TYPE, default=TYPE_YAML): cv.one_of(TYPE_YAML, TYPE_SOFTWARE_VERSION, TYPE_HARDWARE_VERSION, TYPE_DEVICE_NAME, lower=True),
+        cv.Required(CONF_TYPE): cv.one_of(TYPE_SOFTWARE_VERSION, TYPE_HARDWARE_VERSION, TYPE_DEVICE_NAME, lower=True),
     }
 )
 
@@ -25,9 +24,7 @@ async def to_code(config):
     ts = await text_sensor.new_text_sensor(config)
 
     typ = config[CONF_TYPE]
-    if typ == TYPE_YAML:
-        cg.add(hub.set_yaml_text_sensor(ts))
-    elif typ == TYPE_SOFTWARE_VERSION:
+    if typ == TYPE_SOFTWARE_VERSION:
         cg.add(hub.set_software_version_sensor(ts))
     elif typ == TYPE_HARDWARE_VERSION:
         cg.add(hub.set_hardware_version_sensor(ts))
